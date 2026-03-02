53e8e90d7589b28c23834c85837ee517a36f0ddd
Andrew Jameison <arj5881@rit.edu>
2026-03-02 13:13:06 -0500

    Adding media files to project
    We add a bunch of the built in media files so we don't have to go searching for starter material, but users also don't have to download ogre

---
+++ external/OgreDirectories/Materials/Samples/Media/materials/programs/GLSL/Example_TextureArrayVS.glsl
@@ -0,0 +1,16 @@
+OGRE_NATIVE_GLSL_VERSION_DIRECTIVE
+#include <OgreUnifiedShader.h>
+
+OGRE_UNIFORMS(
+uniform mat4 worldViewProj;
+)
+
+MAIN_PARAMETERS
+IN(vec4 vertex, POSITION)
+IN(vec3 uv0, TEXCOORD0)
+OUT(vec3 oUv, TEXCOORD0)
+MAIN_DECLARATION
+{
+	gl_Position = mul(worldViewProj, vertex);
+	oUv = uv0;
+}
